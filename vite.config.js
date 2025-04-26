27a0c35d642eb6b86db0168f4202f0571f6edcd1
siham-tayebi-pp <siham.tayebi-etu@etu.univh2c.ma>
2025-04-26 01:30:49 +0100

    DEPLOYEMENT SETTINGS

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,8 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-    ],
+    ], build: {
+        outDir: 'public/build', // Force ce chemin
+        emptyOutDir: true,
+    }
 });
